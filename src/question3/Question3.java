dbef29b4cb2a0b20515139e1fcaab3862130bb2b
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-19 14:32:45 +0530

    add question 3

---
+++ src/question3/Question3.java
@@ -0,0 +1,23 @@
+package question3;
+
+/*
+ * @author Vivek Sharma
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+
+public class Question3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " % " + b + " = " + (a % b));
+        s.close();
+    }
+}
