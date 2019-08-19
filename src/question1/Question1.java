228f33a8ef79afc6a7fd40bd16bb1169538c4029
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-19 14:22:15 +0530

    add question 1

---
+++ src/question1/Question1.java
@@ -0,0 +1,15 @@
+package question1;
+/*
+ * @author Vivek Sharma
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Welcome, " + name + "!");
+    }
+}
