e6a11b0c18d318e20c0d621740fd9574118d6571
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-19 15:52:25 +0530

    add ConstructorDemo.java and ExecutionDemo.java

--- src/main/ConstructorDemo.java
+++ src/main/ConstructorDemo.java
@@ -1,11 +1,32 @@
 package main;
 
-
 /*
  * @author Vivek Sharma
  * @version 1.0
  */
 
 public class ConstructorDemo {
+    private int number;
+    private String name;
 
+    public ConstructorDemo(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/ExecutionDemo.java
+++ src/main/ExecutionDemo.java
@@ -1,4 +1,9 @@
 package main;
 
 public class ExecutionDemo {
+    public static void main(String[] args) {
+        ConstructorDemo c1 = new ConstructorDemo("Vivek", 45);
+        System.out.println(c1.getName());
+        System.out.println(c1.getNumber());
+    }
 }
